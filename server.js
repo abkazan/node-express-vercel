577cfb217d94301dbb07addbbc038aee9cae77a4
abkazan <abkazan@wisc.edu>
2023-09-20 13:07:31 -0500

    added routing

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
-
+const home = require("./routes/home");
 const app = express();
 
 app.use(express.json()); // for parsing application/json
 
-app.get('/', (req, res) => {
-    res.send("The worldliest of hello's!");
-});
+app.use("/home", home);
+
 
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`listening on port ${port}`));
